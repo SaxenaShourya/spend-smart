d696e724e5c004a90b0de2f67cd40f3df84dc187
Shourya Saxena <saxenashourya000@gmail.com>
2024-05-14 16:16:28 +0530

    feat(backend/user): Add user registration controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,39 @@
+import User from "../models/userModel.js";
 import asyncHandler from "../middlewares/asyncHandler.js";
 
+import generateHash from "../utils/generateHash.js";
+import generateCookie from "../utils/generateCookie.js";
+
+// Controller function to register a new user
 export const registerUser = asyncHandler(async (req, res) => {
-  res.json({ success: "This is a user controller!" });
+  const { username, email, password } = req.body;
+
+  const emailExists = await User.findOne({ email });
+  if (emailExists) {
+    return res
+      .status(400)
+      .json({ error: "Email is already Registered. Please login instead!" });
+  }
+
+  const userNameExists = await User.findOne({ username });
+  if (userNameExists) {
+    return res.status(400).json({ error: "Username is already taken!" });
+  }
+
+  const hashedPassword = await generateHash(password);
+
+  const newUser = new User({ username, email, password: hashedPassword });
+
+  await newUser.save();
+
+  await generateCookie(res, newUser._id);
+  res.status(200).json({
+    message: "User registered successfully. Please verify your email!",
+    user: {
+      _id: newUser._id,
+      username: newUser.username,
+      email: newUser.email,
+      verified: newUser.verified,
+    },
+  });
 });
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,23 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/userController.js";
 
+import {
+  validateEmailAddress,
+  validateUsernameLength,
+  validatePasswordLength,
+} from "../utils/validations.js";
+
+import validate from "../middlewares/validate.js";
+
 const router = Router();
 
-router.route("/").get(registerUser);
+router.route("/").post(
+  validate({
+    username: validateUsernameLength,
+    email: validateEmailAddress,
+    password: validatePasswordLength,
+  }),
+  registerUser
+);
 
 export default router;
