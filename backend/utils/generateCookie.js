33113be4895cf780eeb3272071a013bbd8eff617
Shourya Saxena <saxenashourya000@gmail.com>
2024-05-14 16:14:42 +0530

    feat(backend/utils): Add generateCookie utility

---
+++ backend/utils/generateCookie.js
@@ -0,0 +1,18 @@
+import jwt from "jsonwebtoken";
+
+const generateCookie = (res, userId) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET_KEY, {
+    expiresIn: "30d",
+  });
+
+  res.cookie("session", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV !== "development",
+    sameSite: "strict",
+    maxAge: 30 * 24 * 60 * 60 * 600,
+  });
+
+  return token;
+};
+
+export default generateCookie;
