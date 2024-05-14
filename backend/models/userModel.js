cc484ab864ac9db8635580c97662f8a5d34fc8c0
Shourya Saxena <saxenashourya000@gmail.com>
2024-05-14 15:40:26 +0530

    feat(backend/user): Add user schema

---
+++ backend/models/userModel.js
@@ -0,0 +1,28 @@
+import { Schema, model } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    verified: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = model("User", userSchema);
+
+export default User;
