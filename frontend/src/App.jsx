7cee93ef0944942385b1b3d3e6298a0a9c07eb2a
Shourya Saxena <saxenashourya000@gmail.com>
2024-05-15 15:12:04 +0530

    feat(frontend/components): Add top loading bar component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 
+import TopLoadingBar from "./components/TopLoadingBar";
 import { Home, NotFound } from "./pages";
 
 const App = () => {
   return (
     <>
+      <TopLoadingBar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="*" element={<NotFound />} />
---
+++ frontend/src/components/TopLoadingBar.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import LoadingBar from "react-top-loading-bar";
+
+const TopLoadingBar = () => {
+  return <LoadingBar color="#3B82F6" height={3} />;
+};
+
+export default TopLoadingBar;
