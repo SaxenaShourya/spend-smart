c74f5d9e480784c43568a303d03926ca215fc25d
Shourya Saxena <saxenashourya000@gmail.com>
2024-05-15 15:42:54 +0530

    feat(frontend/components): Update top loader based on state

--- frontend/src/components/TopLoadingBar.jsx
+++ frontend/src/components/TopLoadingBar.jsx
@@ -1,8 +1,22 @@
 import React from "react";
 import LoadingBar from "react-top-loading-bar";
+import { useSelector, useDispatch } from "react-redux";
+
+import { resetLoader } from "../features/loader/loaderSlice";
 
 const TopLoadingBar = () => {
-  return <LoadingBar color="#3B82F6" height={3} />;
+  const progress = useSelector((state) => state.loader.progress);
+
+  const dispatch = useDispatch();
+
+  return (
+    <LoadingBar
+      color="#3B82F6"
+      height={3}
+      progress={progress}
+      onLoaderFinished={() => dispatch(resetLoader())}
+    />
+  );
 };
 
 export default TopLoadingBar;
