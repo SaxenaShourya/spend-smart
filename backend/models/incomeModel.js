67e4031bc9a525e4839487ea20647c6ea75c1ab7
Shourya Saxena <saxenashourya000@gmail.com>
2024-05-15 08:06:50 +0530

    feat(backend/income): Add income schema

---
+++ backend/models/incomeModel.js
@@ -0,0 +1,36 @@
+import { Schema, model } from "mongoose";
+
+const incomeSchema = new Schema(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Income = model("Income", incomeSchema);
+
+export default Income;
