05dc7f3fab669fd0b12e2bd3a2c7cc77711b6d94
Shourya Saxena <saxenashourya000@gmail.com>
2024-05-15 09:54:40 +0530

    feat(backend/expense): Add expense schema

---
+++ backend/models/expenseModel.js
@@ -0,0 +1,36 @@
+import { Schema, model } from "mongoose";
+
+const expenseSchema = new Schema(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Expense = model("Expense", expenseSchema);
+
+export default Expense;
