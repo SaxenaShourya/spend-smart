01b1719976943703b3aedf3fc28c83b0a820a9e1
Shourya Saxena <saxenashourya000@gmail.com>
2024-05-15 15:14:18 +0530

    feat(frontend): Add toast container using react-toastify

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import { ToastContainer, Slide } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 import TopLoadingBar from "./components/TopLoadingBar";
 import { Home, NotFound } from "./pages";
@@ -8,6 +10,14 @@ const App = () => {
   return (
     <>
       <TopLoadingBar />
+      <ToastContainer
+        position="bottom-right"
+        autoClose={2500}
+        pauseOnHover={false}
+        theme="dark"
+        transition={Slide}
+        toastClassName="font-outfit max-w-xs rounded-lg ml-4 sm:ml-0 mb-2"
+      />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="*" element={<NotFound />} />
