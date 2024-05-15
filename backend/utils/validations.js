7b52733e2559e7faf4ba42e84b8856043290709b
Shourya Saxena <saxenashourya000@gmail.com>
2024-05-15 08:12:32 +0530

    feat(backend/utility): Add validations utility for incomes and expenses

--- backend/utils/validations.js
+++ backend/utils/validations.js
@@ -40,3 +40,55 @@ export const validateOTP = (otp) => {
   }
   return;
 };
+
+// Incomes/Expenses validations
+export const validateTitleLength = (title) => {
+  if (title.length < 5) {
+    return "Title must be at least 5 characters long!";
+  }
+  if (title.length > 15) {
+    return "Title should not be more than 15 characters!";
+  }
+  return;
+};
+
+export const validateAmount = (amount) => {
+  if (isNaN(amount) || amount <= 0) {
+    return "Amount must be a positive number!";
+  }
+  return;
+};
+
+export const validateIncomeCategory = (category) => {
+  const allowedCategories = [
+    "salary",
+    "freelance",
+    "investments",
+    "youtube",
+    "rent",
+    "bitcoin",
+    "other",
+  ];
+  if (!allowedCategories.includes(category)) {
+    return "Invalid category!";
+  }
+  return;
+};
+
+export const validateDate = (date) => {
+  const dateRegex = /^\d{4}-\d{2}-\d{2}$/;
+  if (!dateRegex.test(date)) {
+    return "Invalid date format!";
+  }
+  return;
+};
+
+export const validateDescriptionLength = (description) => {
+  if (description.length < 5) {
+    return "Description must be at least 5 characters long!";
+  }
+  if (description.length > 80) {
+    return "Description should not be more than 80 characters!";
+  }
+  return;
+};
