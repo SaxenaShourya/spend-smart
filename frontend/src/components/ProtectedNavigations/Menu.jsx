0708c415931af9e03bbc21c63525dc285cf2055e
Shourya Saxena <saxenashourya000@gmail.com>
2024-05-16 17:42:54 +0530

    feat(frontend/components): Add menu component for top bar

---
+++ frontend/src/components/ProtectedNavigations/Menu.jsx
@@ -0,0 +1,98 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+
+import {
+  Dropdown,
+  DropdownTrigger,
+  DropdownMenu,
+  DropdownItem,
+  Button,
+} from "@nextui-org/react";
+
+import {
+  Menu,
+  Dashboard,
+  Income,
+  Expense,
+  Settings,
+  ShutDown as Logout,
+} from "../../utils/Icons";
+import { openModal } from "../../features/logoutModal/logoutModalSlice";
+
+const Menu = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen((prev) => !prev);
+  };
+
+  return (
+    <Dropdown onClose={() => setIsMenuOpen((prev) => !prev)}>
+      <DropdownTrigger>
+        <Button
+          color="primary"
+          startContent={<Menu />}
+          onClick={toggleMenu}
+          isIconOnly
+        />
+      </DropdownTrigger>
+      <DropdownMenu aria-label="Dropdown Navigations">
+        <DropdownItem
+          key="dashboard"
+          color="primary"
+          startContent={<Dashboard />}
+          description="View your dashboard"
+          onPress={() => navigate("/dashboard")}
+        >
+          Dashboard
+        </DropdownItem>
+
+        <DropdownItem
+          key="incomes"
+          color="success"
+          startContent={<Income />}
+          description="View and Edit your incomes."
+          onPress={() => navigate("/dashboard/incomes")}
+        >
+          Incomes
+        </DropdownItem>
+
+        <DropdownItem
+          key="expenses"
+          color="danger"
+          startContent={<Expense />}
+          description="View and Edit your expenses."
+          onPress={() => navigate("/dashboard/expenses")}
+        >
+          Expenses
+        </DropdownItem>
+
+        <DropdownItem
+          key="settingss"
+          color="secondary"
+          startContent={<Settings />}
+          description="View account Settings."
+          onPress={() => navigate("/dashboard/settings")}
+        >
+          Settings
+        </DropdownItem>
+
+        <DropdownItem
+          key="logout"
+          color="danger"
+          startContent={<Logout />}
+          description="Sign out from your account"
+          onPress={() => dispatch(openModal())}
+        >
+          Logout
+        </DropdownItem>
+      </DropdownMenu>
+    </Dropdown>
+  );
+};
+
+export default Menu;
--- frontend/src/utils/Icons.jsx
+++ frontend/src/utils/Icons.jsx
@@ -1,11 +1,19 @@
-import { RiShieldUserFill, RiLoginCircleLine } from "react-icons/ri";
+import {
+  RiShieldUserFill,
+  RiLoginCircleLine,
+  RiShutDownLine,
+} from "react-icons/ri";
 import { FaPersonSnowboarding, FaCircleUser } from "react-icons/fa6";
 import { PiDotsThreeOutlineDuotone } from "react-icons/pi";
 import { TiHome } from "react-icons/ti";
 import { HiMail } from "react-icons/hi";
 import { BsFillShieldLockFill } from "react-icons/bs";
-import { IoEye, IoEyeOff } from "react-icons/io5";
+import { IoEye, IoEyeOff, IoSettingsOutline } from "react-icons/io5";
 import { FaSnowboarding } from "react-icons/fa";
+import { RxDashboard } from "react-icons/rx";
+import { TbMoneybag } from "react-icons/tb";
+import { BiDollarCircle } from "react-icons/bi";
+import { LuMenu } from "react-icons/lu";
 
 export const Register = RiShieldUserFill;
 export const Login = RiLoginCircleLine;
@@ -18,3 +26,9 @@ export const Password = BsFillShieldLockFill;
 export const Eye = IoEye;
 export const EyeOff = IoEyeOff;
 export const Logout = FaSnowboarding;
+export const Dashboard = RxDashboard;
+export const Income = TbMoneybag;
+export const Expense = BiDollarCircle;
+export const Settings = IoSettingsOutline;
+export const ShutDown = RiShutDownLine;
+export const Menu = LuMenu;
