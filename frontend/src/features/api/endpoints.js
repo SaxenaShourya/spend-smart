df13919b0fcc5e37c8cbf213823ca8457c8e8f14
Shourya Saxena <saxenashourya000@gmail.com>
2024-05-20 14:37:31 +0530

    feat(frontend): Add backend URL environment variables in endpoints file

--- frontend/src/features/api/endpoints.js
+++ frontend/src/features/api/endpoints.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "/api/v1";
+export const BASE_URL = import.meta.env.VITE_BACKEND_API_URL || "/api/v1"; // ⚠️⚠️⚠️ for development use - "/api/v1"
 export const USERS_URL = "/users";
 export const INCOMES_URL = "/incomes";
 export const EXPENSES_URL = "/expenses";
