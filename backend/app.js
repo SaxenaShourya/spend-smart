a326019c02508cdd69b32640d1a1b060afd06e0f
Shourya Saxena <saxenashourya000@gmail.com>
2024-05-20 14:47:50 +0530

    feat(backend): Integrate CORS policy

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import connectDB from "./config/db.js";
 
@@ -24,6 +25,27 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+// CORS configurations
+app.use(
+  cors({
+    origin: "https://spend-smart-dev.vercel.app",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+app.use((req, res, next) => {
+  const allowedOrigins = ["https://spend-smart-dev.vercel.app"];
+  const origin = req.headers.origin;
+
+  if (allowedOrigins.includes(origin)) {
+    next();
+  } else {
+    res.status(404).json({ error: "Blocked by cors!" });
+    return;
+  }
+});
 
 // Routes
 app.use("/api/v1/users", userRoutes);
