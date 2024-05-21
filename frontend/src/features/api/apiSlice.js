f7458fb40154474e47f6cfdfd3cf5a66eb90d148
Shourya Saxena <saxenashourya000@gmail.com>
2024-05-21 15:02:53 +0530

    feat(frontend): Add cookies in fetch base query

--- frontend/src/features/api/apiSlice.js
+++ frontend/src/features/api/apiSlice.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { BASE_URL } from "./endpoints";
 
-const baseQuery = fetchBaseQuery({ baseUrl: BASE_URL });
+const baseQuery = fetchBaseQuery({ baseUrl: BASE_URL, credentials: "include" });
 
 export const apiSlice = createApi({
   baseQuery,
