1545dd3b2f9e01f2376e5d1ff489f2dbd623115b
Shourya Saxena <saxenashourya000@gmail.com>
2024-05-21 15:29:40 +0530

    feat(frontend): Remove console logs in production environment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import removeConsole from "vite-plugin-remove-console";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => ({
+  plugins: [react(), mode === "production" && removeConsole()].filter(Boolean),
   server: {
     host: true,
     strictPort: true,
@@ -15,4 +16,4 @@ export default defineConfig({
       },
     },
   },
-});
+}));
